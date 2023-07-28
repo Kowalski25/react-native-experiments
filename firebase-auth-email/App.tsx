a21ac761a590235cba89777cdec9327abe7c44fb
Kowalski25 <it.felixarhelacbang@gmail.com>
2023-07-28 18:37:09 +0800

    added firebase auth with email signin/registration project

---
+++ firebase-auth-email/App.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./src/screens/Login";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen
+          name='Login'
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
